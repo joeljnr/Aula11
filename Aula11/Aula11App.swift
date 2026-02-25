c42f7304d7ae7e7196968b738b737aa1a9b63fb9
Joel Júnior <joel.asj2@gmail.com>
2026-02-25 09:58:49 -0300

    commit inicial

---
+++ Aula11/Aula11App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula11App.swift
+//  Aula11
+//
+//  Created by A2Works on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Aula11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
